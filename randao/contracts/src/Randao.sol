89e276e5bc3b55489c197f524814dbe92f3920ef
Yi Sun <yi-sun@users.noreply.github.com>
2023-01-29 05:05:16 -0500

    randao: Refactor in preparation for fix

--- randao/contracts/src/Randao.sol
+++ randao/contracts/src/Randao.sol
@@ -1,12 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+// WARNING! This smart contract and the associated zk-SNARK verifiers have not been audited.
+// DO NOT USE THIS CONTRACT FOR PRODUCTION
+pragma solidity ^0.8.12;
 
-import "@axiom/src/Axiom.sol";
+import "./IAxiomV0.sol";
 
 contract Randao {
-    address axiomAddress;
+    address private axiomAddress;
 
-    // mapping between blockNumber (32) and prevRandao (256)
+    // mapping between blockNumber and prevRandao 
     mapping(uint32 => uint256) public prevRandaos;
 
     event RandaoProof(uint32 blockNumber, uint256 prevRandao);
@@ -16,28 +18,26 @@ contract Randao {
     }
 
     function verifyRandao(
-        Axiom.BlockHashWitness memory blockProof,
+        IAxiomV0.BlockHashWitness memory witness,
         uint256 prevRandao,
         bytes calldata prevBlockRlp,
         bytes calldata postBlockRlp
     ) public {
-        require(
-            Axiom(axiomAddress).isBlockHashValid(
-                blockProof.blockNumber,
-                blockProof.claimedBlockHash,
-                blockProof.prevHash,
-                blockProof.numFinal,
-                blockProof.merkleProof
-            ),
-            "Invalid block hash in cache"
-        );
+        if (block.number - witness.blockNumber <= 256) {
+            require(IAxiomV0(axiomAddress).isRecentBlockHashValid(witness.blockNumber, witness.claimedBlockHash),
+                    "Block hash was not validated in cache");
+        } else {
+            require(IAxiomV0(axiomAddress).isBlockHashValid(witness),
+                    "Block hash was not validated in cache");
+        } 
+
         require(
             keccak256(abi.encodePacked(prevBlockRlp, prevRandao, postBlockRlp)) ==
-                blockProof.claimedBlockHash,
+                witness.claimedBlockHash,
             "Block hash does not match witness hash"
         );
 
-        emit RandaoProof(blockProof.blockNumber, prevRandao);
-        prevRandaos[blockProof.blockNumber] = prevRandao;
+        prevRandaos[witness.blockNumber] = prevRandao;
+        emit RandaoProof(witness.blockNumber, prevRandao);        
     }
 }
--- randao/contracts/test/Randao.t.sol
+++ randao/contracts/test/Randao.t.sol
@@ -3,72 +3,48 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "@axiom/src/AxiomDemo.sol";
+import "../src/IAxiomV0.sol";
 import "../src/Randao.sol";
-import "../lib/YulDeployer.sol";
-
-uint32 constant testBlockNumber = 15537393;
-uint32 constant testPrevBlockNumber = 15537152;
-bytes32 constant testBlockHash = bytes32(hex"55b11b918355b1ef9c5db810302ebad0bf2544255b530cdce90674d5887bb286");
-bytes32 constant testPrevHash = bytes32(hex"a9d44102e2414cef64b15e650b841ab630b772b733e30d1019d91d168e415468");
-bytes32 constant testRoot = bytes32(hex"5425b718c94550877486f0b9b52d01425c1e6262e5d6a5402e9eb8344e4239c7");
-uint32 constant testNumFinal = 0;
 
 contract RandaoTest is Test {
-    AxiomDemo public axiom;
-    YulDeployer yulDeployer;    
-    Randao app;
+    address AXIOM_ADDRESS = 0x01d5b501C1fc0121e1411970fb79c322737025c2;
 
-    function setUp() public {
-        yulDeployer = new YulDeployer();
-        address axiomVerifierAddress = address(yulDeployer.deployContract("mainnet_10_7"));
-
-        axiom = new AxiomDemo(
-            axiomVerifierAddress,
-            axiomVerifierAddress,
-            testPrevBlockNumber, 
-            keccak256(abi.encodePacked(testPrevHash, testRoot, testNumFinal))        
-        );
-        app = new Randao(address(axiom));
-    }
-
-    function testVerifyRandao() public {
-        // Import test proof and instance calldata
-        string[] memory prevInputs = new string[](2);
-        prevInputs[0] = "cat";
-        prevInputs[1] = "test/data/testPrev.calldata";
-        bytes memory blockRlpPrev = vm.ffi(prevInputs);
+   function testVerifyRandaoRecent() public {
+        string memory MAINNET_RPC_URL = string.concat("https://mainnet.infura.io/v3/", vm.envString("INFURA_ID"));
+        vm.createSelectFork(MAINNET_RPC_URL, 16_509_500);
 
         // Import test proof and instance calldata
-        string[] memory postInputs = new string[](2);
-        postInputs[0] = "cat";
-        postInputs[1] = "test/data/testPost.calldata";
-        bytes memory blockRlpPost = vm.ffi(postInputs);
+        string[] memory inputs = new string[](2);
+        inputs[0] = "cat";
+        inputs[1] = "test/data/testPrev.calldata";
+        bytes memory prev = vm.ffi(inputs);
 
-        // Prepare witness data for Uniswap TWAP proof.
-        // Note that only the claimed block hash is checked in the test.
-        bytes32[10] memory blockMerkleProof = [
-            bytes32(0x2b3ea3cd4befcab070812443affb08bf17a91ce382c714a536ca3cacab82278b),
-            bytes32(0xb2963b40c8811b7659c2425d357daa4165c5710493ab4cf0293d30d98a621c53),
-            bytes32(0x7d0ee8935eb7043fa21a29a90a734165170dd8aafbec8628c3d94b006a8efb75),
-            bytes32(0x6296f8a7f7225045ea4e812d5fc4352861d1069eaef2ff7896adc2445803eee6),
-            bytes32(0xf7b917c02728fd0513d726200a4169b9c3558a0b1b86d01f9a6d85c4494e4901),
-            bytes32(0x933c8f3e40ca3f46a8c4d64557ff7d8b9b7007890d86e01a7de68cc27dba4bdc),
-            bytes32(0xc5a9564ad180dfc461af09fb31d537bcca46f71959041bd572c60b847e171b5b),
-            bytes32(0x456fb200c357e2247a22a43756c7775a1a7a899024c18686673e003817eec534),
-            bytes32(0xb11f638166e5dbfea2261971c9d4a8a2008eea043cd4b1074f8ad727b1f14ce6),
-            bytes32(0x3b506100b1b0de2e657521e4c43d1a41dfeb4ac189139de5d7f4d99fa0672acb)
-        ];
-        Axiom.BlockHashWitness memory block = Axiom.BlockHashWitness({
-            blockNumber: testBlockNumber,
-            claimedBlockHash: testBlockHash,
-            prevHash: testPrevHash,
-            numFinal: 0,
-            merkleProof: blockMerkleProof
+        string[] memory inputs2 = new string[](2);
+        inputs2[0] = "cat";
+        inputs2[1] = "test/data/testPrev.calldata";
+        bytes memory post = vm.ffi(inputs2);        
+
+        IAxiomV0.BlockHashWitness memory testBlock = IAxiomV0.BlockHashWitness({
+            blockNumber: 16509301,
+            claimedBlockHash: bytes32(0x034ca3921f2ab605c8681288ba4c9818978a12e69c57e82350301fb58e1a9a6b),
+            prevHash: bytes32(0xf21f9ac46b21ce128bf245ac8c5dcd12ab1bf6a0cb0e3c7dc4d33cc8871d8ab3),
+            numFinal: 1024,
+            merkleProof: [bytes32(0x3b4e49db58e4dab1931689ab67e24eb79660f2661034280bf8f4480071294456),
+                          bytes32(0xc7e1e977b5d68588e8643956ff13a20cf95c4a76e6ffb168d8faca06413d8c45),
+                          bytes32(0xaa558831681ec38ab0face0d6eab566ae490d8bdf00e92f27d992836c10372d3),
+                          bytes32(0x69384d455682d10dbc617296b17a8715d109d1b74ab74037d48e521122048810),
+                          bytes32(0x62d103378fc33e7f8578480846a0b5634c083167eb621bf0122cd312e34862df),
+                          bytes32(0x7f3a0dfb38decbb536032102f62f6786dc5c7ac3ed73b283bbc73ba3ea07406b),
+                          bytes32(0x8b3e437e20a1d5da7d018acfec8129a3071085fb2a14c48712e12d13ecb16014),
+                          bytes32(0xbe109e17256d615dc1fa6e241fa424247ede44bdadc36fa48fbfe71895f95d5d),
+                          bytes32(0x3afb75397f28a7fbd51498e7a109865e34edf93ecce9ba4686d7d0fb7b86c63b),
+                          bytes32(0x2953bc9d2dfc756b8d46e849da5971dbe1989603b06b7235627529d2e0e5df1d)]
         });
         uint256 prevRandao = 0x4cbec03dddd4b939730a7fe6048729604d4266e82426d472a2b2024f3cc4043f;
 
-        // Call verify function in app
-        app.verifyRandao(block, prevRandao, blockRlpPrev, blockRlpPost);
-    }
+        Randao randao = new Randao(AXIOM_ADDRESS);
+        randao.verifyRandao(testBlock, prevRandao, prev, post);
+        require(randao.prevRandaos(testBlock.blockNumber) == prevRandao, 
+                "prevRandao not verified");
+    }    
 }
