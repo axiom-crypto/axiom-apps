da9d4a2a319b6067480ad0c3e213fc7131d6fc2b
Barnyard <barneychambers@hotmail.com>
2023-08-17 23:34:08 +0100

    created inferface for Randao.sol. I followed the same path for import… (#8)
    * created inferface for Randao.sol. I followed the same path for importing IAxiom.sol as in the Randao.sol contract, but I believe it might be incorrect, since is seems like the appropriate files are currently here: https://github.com/axiom-crypto/axiom-v1-contracts/contracts/interfaces/IAxiomV1.sol
    
    * removed onlyOwner function from interface

---
+++ randao/src/interfaces/IRandao.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IAxiomV1} from "axiom-contracts/contracts/interfaces/IAxiomV1.sol";
+
+interface IRandao {
+    function prevRandaos(uint32 blockNumber) external view returns (uint256);
+    function axiomAddress() external view returns (address);
+    function mergeBlock() external view returns (uint32);
+
+    function verifyRandao(IAxiomV1.BlockHashWitness calldata witness, bytes calldata header) external;
+}
